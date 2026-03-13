349148814093ae077d8b2672d263fbbab47abf58
Harryqin99 <harryqin1999@gmail.com>
2026-03-13 11:10:49 +1100

    Initial commit

---
+++ src/main/java/org/fxtracker/FxTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.fxtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fxtracker/FxTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fxtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
